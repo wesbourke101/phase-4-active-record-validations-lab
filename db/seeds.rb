f7c382ccdb54dd20f4770f6daa2ff3eabff2f3d2
wesley <wesbourkeavl@gmail.com>
2022-05-01 17:23:23 -0400

    commit tree 7c1498c1f9fb0649c3b5e1d4283534f0ff263b9f

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Author.create(name: "Wes", phone_number: "8282314125")
+# Author.create(name: "Tom", phone_number: "8282938746")
+# Author.create(name: "Adam", phone_number: "4783976524")
+# Author.create(name: "Kristen", phone_number: "9087654321")
+
+# Post.create(title: "name", content: "this and that", summary: "hello this is alot of text so enjoy the film. and enjoy this as well.", category: "Action")
+# Post.create(title: "Another one", content: "that too", summary: "sdkasjdkfjadskfjadksfjadjfkajdfakdfjljdasjfkajd afja.", category: "Action")
+# Post.create(title: "jim", content: "other things", summary: "hello this is alot of text so enjoy the film. and enjoy this as well.", category: "Not Action")
+# Post.create(title: "Will", content: "That good good", summary: "hello this is alot of text so enjoy the film. and enjoy this as well.", category: "Drama")
+# Post.create(title: "LastONE", content: "This is the last one in a series", summary: "hello this is alot of text so enjoy the film. and enjoy this as well.", category: "More")
